c2bd4228e3342d1205254984d3141b089e0da3f7
Yeabsira <yabahane@gmail.com>
2025-03-16 18:12:00 +0300

    tesitng

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import HeroSection from "./components/Screen/HomePage/HeroSection";
 import LoadingScreen from "./components/Screen/LoadingPage/LoadingPage";
 
 export default function App() {
-  return (
+  return ( 
     <main className="w-screen h-screen">
       <HeroSection />
       <LoadingScreen />
